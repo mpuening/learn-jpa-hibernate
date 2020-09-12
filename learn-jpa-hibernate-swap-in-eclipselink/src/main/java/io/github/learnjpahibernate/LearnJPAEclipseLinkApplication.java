4bd42ed86f6f7bb930f87016936b5d6b81c35ace
Michael Puening <mpuening@gmail.com>
2020-09-12 01:39:54 -0400

    Update EclipseLink and OpenJPA dependencies

--- learn-jpa-hibernate-swap-in-eclipselink/src/main/java/io/github/learnjpahibernate/LearnJPAEclipseLinkApplication.java
+++ learn-jpa-hibernate-swap-in-eclipselink/src/main/java/io/github/learnjpahibernate/LearnJPAEclipseLinkApplication.java
@@ -1,8 +1,12 @@
 package io.github.learnjpahibernate;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnJPAEclipseLinkApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJPAEclipseLinkApplication.class, args);
+	}
 }
