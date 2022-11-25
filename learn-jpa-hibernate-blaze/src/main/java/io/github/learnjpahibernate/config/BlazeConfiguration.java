8dabbd4c88d261ee0ca4ec1e50c01a0825561eef
Michael Puening <mpuening@gmail.com>
2022-11-25 11:12:00 -0500

    Update to Spring Boot 3.0.0

--- learn-jpa-hibernate-blaze/src/main/java/io/github/learnjpahibernate/config/BlazeConfiguration.java
+++ learn-jpa-hibernate-blaze/src/main/java/io/github/learnjpahibernate/config/BlazeConfiguration.java
@@ -1,25 +1,17 @@
 package io.github.learnjpahibernate.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.blazebit.persistence.Criteria;
 import com.blazebit.persistence.CriteriaBuilderFactory;
-import com.blazebit.persistence.spi.CriteriaBuilderConfiguration;
 
 import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 public class BlazeConfiguration {
-
-	@Autowired
-	protected EntityManagerFactory entityManagerFactory;
-
 	@Bean
-	public CriteriaBuilderFactory criteriaBuilderFactory() {
-		CriteriaBuilderConfiguration config = Criteria.getDefault();
-		CriteriaBuilderFactory criteriaBuilderFactory = config.createCriteriaBuilderFactory(this.entityManagerFactory);
-		return criteriaBuilderFactory;
+	public CriteriaBuilderFactory criteriaBuilderFactory(EntityManagerFactory entityManagerFactory) {
+		return Criteria.getDefault().createCriteriaBuilderFactory(entityManagerFactory);
 	}
 }
